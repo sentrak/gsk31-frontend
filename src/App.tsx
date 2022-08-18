52ebc32f68a822778b0d5e1a692f7b0ba881c23b
sentrak <santiago.jimenez.raigosa@hotmail.com>
2022-08-18 15:02:48 -0500

    SSH COOL

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload eso mismo 2.0.
+          Edit <code>src/App.tsx</code> and save to reload eso mismo 2.0. ssh
         </p>
         <a
           className="App-link"
